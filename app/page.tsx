4596770325a490619e01d8959b2edb307964e503
Selim <hello@selimmersive.com>
2024-07-10 14:02:19 +0100

    i remove the component for facebook

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import ComponentWithSearchParams from "@/components/ComponentWithSearchParams";
 import HeaderPage from "@/components/Header";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
     <Suspense>
-      <ComponentWithSearchParams />
       <HeaderPage />
     </Suspense>
   );
