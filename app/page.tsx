4e912d7c3eaa44bef75951d2911d1f512f7e13a1
Selim <hello@selimmersive.com>
2024-07-10 13:45:15 +0100

    i add parameters for facebook

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
 import HeaderPage from "@/components/Header";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Home() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const openInBrowser = searchParams.get('openInBrowser');
+    if (openInBrowser === 'true') {
+      const newSearchParams = new URLSearchParams(searchParams.toString());
+      newSearchParams.delete('openInBrowser');
+      const newUrl = `${window.location.origin}${pathname}${newSearchParams.toString() ? `?${newSearchParams.toString()}` : ''}`;
+
+      window.open(newUrl, '_blank');
+    }
+  }, [pathname, searchParams]);
+
   return (
     <Suspense>
       <HeaderPage />
